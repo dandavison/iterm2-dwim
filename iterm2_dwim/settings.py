e05328cac5a89c404aaf4d8e07ed62810408dac8
Dan Davison <dandavison7@gmail.com>
2017-09-22 21:57:47 -0700

    Local settings

--- iterm2_dwim/settings.py
+++ iterm2_dwim/settings.py
@@ -2,8 +2,7 @@
 # relevant executable file on your system.
 
 # For example, if you have installed Emacs from homebrew, this should probably be
-# emacsclient = '/usr/local/bin/emacsclient'
-emacsclient = None
+emacsclient = '/usr/local/bin/emacsclient'
 
 # For example, if you've installed Sublime from the .dmg, this should probably be
 # sublime = '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
